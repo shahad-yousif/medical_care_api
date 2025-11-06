d0539e4b2a35a1f8572647c69078927abf2984dc
shahad-yousif <shahad@gilgabytes.com>
2025-11-06 15:37:39 +0300

    SY create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,12 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: user, status: :created
+      render json: UserSerializer.new(user), status: :created
     else
-      render json: { errors: user.errors }, status: :unprocessable_entity
+      render json: {
+        message: 'User creation failed',
+        errors: user.errors.full_messages
+      }, status: :unprocessable_entity
     end
   end
 
