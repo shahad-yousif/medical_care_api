315de0f3e7de6ca7d9280e22921796504bb10c9a
shahad-yousif <shahad@gilgabytes.com>
2025-11-10 16:58:02 +0300

    SY fix issue

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -305,6 +305,14 @@ Devise.setup do |config|
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
 
+  # Source - https://stackoverflow.com/a
+# Posted by Raymundus
+# Retrieved 2025-11-10, License - CC BY-SA 3.0
+
+
+  config.navigational_formats = []
+
+
   # ==> Configuration for :registerable
 
   # When set to false, does not sign a user in automatically after their password is
